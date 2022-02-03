b9d3af2f21f09e9d9593ccacb1f95ab9ca408610
tadzyla <tadas.zvinklys@gmail.com>
2022-02-03 09:56:24 +0000

    adding jquery for buttons

--- app.js
+++ app.js
@@ -4,3 +4,31 @@ timeLine.to('.slider', { y: "-100%", duration: 1.5, delay: 0.5 });
 timeLine.to('.intro', { y: '-100%', duration: 1 }, '-=1');
 timeLine.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
 timeLine.fromTo('.big-text', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+
+$(document).ready(function() {
+
+    $('#logo').on({
+        mouseenter: function() {
+            $(this).css({
+                backgroundColor: 'rgb(197, 153, 140)',
+                color : 'black',
+            });
+        },
+
+        mouseleave: function() {
+            $(this).css('backgroundColor', '');
+            $(this).css('color', '');
+        }
+    });
+
+    $('.navlinks li').on({
+        mouseenter: function() {
+            $(this).css('backgroundColor', 'rgb(197, 153, 140)');
+            $(this).css('color', 'black');
+        },
+        mouseleave: function() {
+            $(this).css('backgroundColor', '');
+            $(this).css('color', '')
+        }
+    });
+});
